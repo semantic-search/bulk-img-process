23180e99dd0ebe5d656272b7b0a4257a595e1ab8
Akshay Ashok <aks28id@gmail.com>
2020-08-19 22:57:18 +0530

    initial commit

---
+++ send_to_index.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from kafka import KafkaProducer
+import json
+import base64
+
+from topics import topicsList
+
+producer = KafkaProducer(
+    bootstrap_servers=["40.88.35.171:9092"],
+    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+)
+
+images_path = list(Path('images').glob('*.*'))
+
+images_name = [ p.stem for p in images_path]
+
+for topic, topicDict in topicsList.items():
+    endpoint, topicName = topicDict.items()
+    endpoint = endpoint[1] 
+    topicName = topicName[1]
+    print(topicName, endpoint)
+
+    for idx, image in enumerate(images_path):
+        
+        with open(image, "rb") as image_file:
+            encoded_image = base64.b64encode(image_file.read())
+        #send to consumer
+        message = {
+            'image_id' : images_name[idx], 
+             'data' : encoded_image
+        }
+        
+        future = producer.send(TOPIC_NAME, value=message)
+
+producer.flush()
+
---
+++ topics.py
@@ -0,0 +1,10 @@
+topicsList = {
+    "EASY_OCR" : {
+        "endpoint" : "http://52.188.166.61:80/easyocr/",
+        "name" : "EASY_OCR"
+    },
+    "DENSECAP" : {
+        "endpoint" : "http://52.188.166.61:7000/uploadfile/",
+        "name" : "DENSECAP"
+    }
+}
