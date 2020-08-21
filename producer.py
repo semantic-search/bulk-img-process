44a5227710839ffd503683aeac8aa19cb110a008
Akshay Ashok <aks28id@gmail.com>
2020-08-22 00:37:39 +0530

    fix

--- producer.py
+++ producer.py
@@ -13,10 +13,8 @@ producer = KafkaProducer(
 
 TOPIC_NAME = "EASY_OCR"
 
-topic = sys.argv[1]
-
-if topic:
-    TOPIC_NAME = topic
+if sys.argv[1]:
+    TOPIC_NAME = sys.argv[1]
 
 print(f"topic name : {TOPIC_NAME}")
 
