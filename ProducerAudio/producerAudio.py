89918f5446186f30d3c7f9dfc8d86a3afa05ec6c
Akshay Ashok <aks28id@gmail.com>
2020-08-22 20:05:29 +0530

    audio added

---
+++ ProducerAudio/producerAudio.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from kafka import KafkaProducer
+import json
+import base64
+import sys
+
+from topics import topicsList
+
+producer = KafkaProducer(
+    bootstrap_servers=["40.88.35.171:9092"],
+    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+)
+
+TOPIC_NAME = "MAX_AUDIO"
+
+if len(sys.argv) > 1:
+    TOPIC_NAME = sys.argv[1]
+
+print(f"topic name : {TOPIC_NAME}")
+
+audios_path = list(Path('audios').glob('*.*'))
+
+audios_name = [p.name for p in audios_path]
+
+
+for idx, image in enumerate(audios_path):
+
+    with open(image, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode('ascii')
+    # send to consumer
+    message = {
+        'audio_id': audios_name[idx],
+        'data': encoded_image
+    }
+
+    producer.send(TOPIC_NAME, value=message)
+    producer.flush()
